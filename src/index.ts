8df5af68882dd138d0d41404366dff7f83c0b842
Pau Joan <pregonipaw@hotmail.com>
2020-12-24 18:53:44 +0100

    added api call in index.

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,22 @@
-/* eslint-disable @typescript-eslint/no-var-requires */
 /**
  * @file Mounts an Express http server.
  * @author PawFV <https://github.com/PawFV>
  */
-import bodyParser from 'body-parser'
-import cors from 'cors'
+import cors = require('cors')
 import express from 'express'
+import { apiCall } from './fetch'
+
 const app = express()
 const port = 3000
 
-app.use(cors() as any)
-app.use(bodyParser.json())
+app.use(cors())
+app.use(express.json())
 
 // routes
+// eslint-disable-next-line @typescript-eslint/no-var-requires
 app.use('/users', require('./routes/users'))
 
 app.get('/', (req, res) => res.send('Hello World!'))
 app.listen(port, () => console.log(`App listening on localhost:${port} port!`))
+
+apiCall.getUsers().then(({ data }) => console.log(data))
